03bb323602daae3523f6c79eb08d90bc6cc5731b
Sunny Kumar <sunny.k@thecodebucket.com>
2026-05-15 15:37:58 +0530

    fix no found

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   assetsInclude: ['**/*.pdf'],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    // Ensure small chunks are not inlined as base64 to keep chunks clean
+    assetsInlineLimit: 4096,
+  },
 });
